95b365bc5db3a6613f1b2e93e55ce8f4e04fc407
Nirmal Samaranayake <nirmal.swkc@outlook.com>
2021-06-26 23:01:13 +0800

    create database and adding master data

---
+++ CGZT.School.Demo.Database/Tables/Demo_T_Student.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [cgzt].[Demo_T_Student]
+(
+	[Id] int IDENTITY(1,1) NOT NULL,
+	[Name] nvarchar(100) NOT NULL,
+	[Email] nvarchar(200) NOT NULL UNIQUE,
+	[IsSuspend] Bit Not NULL DEFAULT 'false',
+	[CreatedBy] nvarchar(100) NOT NULL,
+	[CreatedAt] datetime2 NOT NULL DEFAULT getdate(),
+	[UpdatedBy] nvarchar(100) NULL,
+	[UpdatedAt] datetime2 NULL,
+	CONSTRAINT PK_Demo_T_Student PRIMARY KEY (Id)
+)
---
+++ CGZT.School.Demo.Database/Tables/Demo_T_TeacherStudentMapping.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [cgzt].[Demo_T_TeacherStudentMapping]
+(
+	[Id] int IDENTITY(1,1) NOT NULL,
+	[Demo_T_TeacherId] int NOT NULL,
+	[Demo_T_StudentId] int NOT NULL,
+	[CreatedBy] nvarchar(100) NOT NULL,
+	[CreatedAt] datetime2 NOT NULL DEFAULT getdate(),
+	[UpdatedBy] nvarchar(100) NULL,
+	[UpdatedAt] datetime2 NULL,
+	CONSTRAINT PK_Demo_T_TeacherStudentMapping PRIMARY KEY (Id),
+	CONSTRAINT FK_TeacherStudentMappingTeacherId FOREIGN KEY  (Demo_T_TeacherId) REFERENCES [cgzt].Demo_T_Teacher(Id),
+    CONSTRAINT FK_TeacherStudentMappingStudentId FOREIGN KEY  (Demo_T_StudentId) REFERENCES [cgzt].Demo_T_Student(Id),
+    CONSTRAINT UC_TeacherStudentMapping UNIQUE  (Demo_T_TeacherId, Demo_T_StudentId)
+)
